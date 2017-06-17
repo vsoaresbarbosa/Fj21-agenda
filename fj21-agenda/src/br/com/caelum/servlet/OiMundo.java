72b34d7daf4985c972a8cfaa0fcd7e03674a30a4
web6551 <web6551@caelum144-02>
2017-06-17 15:14:15 -0300

    insercao de dados no banco atraves da web codado em html e java

---
+++ fj21-agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,27 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet ("/oi")
+public class OiMundo extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		PrintWriter out = arg1.getWriter();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Primeira servlet </title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1> Oi Mundo servlet </h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
